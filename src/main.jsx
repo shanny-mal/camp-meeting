91426e27906124545c5a8fc859f71675c527dcd6
Shannon Malvin Chipezeze <malvinshanny@gmail.com>
2025-05-08 09:50:43 +0200

    changed Index.css to index.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "./Index.css";
+import "./index.css";
 
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
